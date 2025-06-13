7884f9804726e963e06740e0f779fcbe9a5e044c
Muhammad Arshman <itxarshman@gmail.com>
2025-06-13 20:34:57 +0500

    Update filemanager.py

--- filemanager.py
+++ filemanager.py
@@ -1,10 +1,4 @@
-from telegram.ext import CallbackQueryHandler
+from aiogram.types import Message
 
-def handle_file_manager(update, context):
-    query = update.callback_query
-    query.answer()
-    server_id = query.data.split('_')[1]
-    query.edit_message_text(f"📁 File Manager for server {server_id}")
-
-def setup_file_handlers(application):
-    application.add_handler(CallbackQueryHandler(handle_file_manager, pattern="^file_"))
+async def handle_file_manager(message: Message, server_data):
+    await message.answer("📂 File Manager: Coming Soon")
