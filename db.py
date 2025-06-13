d9911c4f61123e447f384fcb728f500efc65f383
Muhammad Arshman <itxarshman@gmail.com>
2025-06-13 20:52:25 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 from bson import ObjectId
+from config import MONGO_URI
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(MONGO_URI)
 db = client["server_manager"]
 servers_collection = db["servers"]
 
@@ -15,10 +16,16 @@ def get_server_by_id(server_id):
     return servers_collection.find_one({"_id": ObjectId(server_id)})
 
 def update_server_name(server_id, new_name):
-    servers_collection.update_one({"_id": ObjectId(server_id)}, {"$set": {"name": new_name}})
+    servers_collection.update_one(
+        {"_id": ObjectId(server_id)},
+        {"$set": {"name": new_name}}
+    )
 
 def update_server_username(server_id, new_username):
-    servers_collection.update_one({"_id": ObjectId(server_id)}, {"$set": {"username": new_username}})
+    servers_collection.update_one(
+        {"_id": ObjectId(server_id)},
+        {"$set": {"username": new_username}}
+    )
 
 def delete_server_by_id(server_id):
     servers_collection.delete_one({"_id": ObjectId(server_id)})
