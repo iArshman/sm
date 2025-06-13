d1c8dd0f8b45804c24f855fefa3084b3aa4b2888
Muhammad Arshman <itxarshman@gmail.com>
2025-06-13 23:20:02 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,31 +1,31 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 from config import MONGO_URI
 
-client = MongoClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URI)
 db = client["server_manager"]
 servers_collection = db["servers"]
 
-def add_server(data):
-    servers_collection.insert_one(data)
+async def add_server(data):
+    await servers_collection.insert_one(data)
 
-def get_servers():
-    return list(servers_collection.find())
+async def get_servers():
+    return await servers_collection.find().to_list(None)
 
-def get_server_by_id(server_id):
-    return servers_collection.find_one({"_id": ObjectId(server_id)})
+async def get_server_by_id(server_id):
+    return await servers_collection.find_one({"_id": ObjectId(server_id)})
 
-def update_server_name(server_id, new_name):
-    servers_collection.update_one(
+async def update_server_name(server_id, new_name):
+    await servers_collection.update_one(
         {"_id": ObjectId(server_id)},
         {"$set": {"name": new_name}}
     )
 
-def update_server_username(server_id, new_username):
-    servers_collection.update_one(
+async def update_server_username(server_id, new_username):
+    await servers_collection.update_one(
         {"_id": ObjectId(server_id)},
         {"$set": {"username": new_username}}
     )
 
-def delete_server_by_id(server_id):
-    servers_collection.delete_one({"_id": ObjectId(server_id)})
+async def delete_server_by_id(server_id):
+    await servers_collection.delete_one({"_id": ObjectId(server_id)})
