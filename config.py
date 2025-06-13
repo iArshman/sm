37ba0065a5b8d77289cbdbfbd0f8e495fc95aa76
Muhammad Arshman <itxarshman@gmail.com>
2025-06-13 17:13:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 MONGO_URI = "mongodb+srv://irexanon:xUf7PCf9cvMHy8g6@rexdb.d9rwo.mongodb.net/?retryWrites=true&w=majority&appName=RexDB"
-BOT_TOKEN = "7640490435:AAHRyMZ52ShAASHbhrfc8UymKkFXQPy-sxQ"
+BOT_TOKEN = "8167520002:AAEHfTsi8SBJQU2V8mOVI15MG2X8fvKQZoU"
 ADMIN_IDS = [7405203657]  # Your Telegram user ID
 SSH_TIMEOUT = 5  # seconds
