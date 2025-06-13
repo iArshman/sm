f0ed0a5a96cbf00ef94cdee14881b2c3019ce2c5
Muhammad Arshman <itxarshman@gmail.com>
2025-06-13 20:34:42 +0500

    Update botmanager.py

--- botmanager.py
+++ botmanager.py
@@ -1,10 +1,4 @@
-from telegram.ext import CallbackQueryHandler
+from aiogram.types import Message
 
-def handle_bot_manager(update, context):
-    query = update.callback_query
-    query.answer()
-    server_id = query.data.split('_')[1]
-    query.edit_message_text(f"🤖 Bot Manager for server {server_id}")
-
-def setup_bot_handlers(application):
-    application.add_handler(CallbackQueryHandler(handle_bot_manager, pattern="^bot_"))
+async def handle_bot_manager(message: Message, server_data):
+    await message.answer("🤖 Bot Manager: Coming Soon")
